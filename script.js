e3859af6966b08795aafb1b5379a79da1135e376
Javad-EB <91270996+Javad-EB@users.noreply.github.com>
2022-11-08 09:10:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById('popup');
+const modal = document.getElementById('popup');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -8,84 +8,150 @@ window.onclick = function (event) {
     }
 }
 
+//I cant use this class! I have a Problem
 class UI {
-    static showAlert(message, className){
-        const div = document.createElement('div');
-        div.className = `alert ${className}`;
-        div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.box-form');
-        const right = document.querySelector('.right');
-        const login = document.querySelector('.login-form');
-        const msg = document.querySelector('.msg');
-        login.insertBefore(div, msg);
-        
-        //Vanish in 3 Seconds
+    static showAlert(message, idName) {
+        const msgDiv = document.getElementById('idName');
+        msgDiv.textContent = message;
+        //Vanish in 4 Seconds
         setTimeout(() => {
-            document.querySelector('.alert').remove()
-        }, 2000);
+            document.querySelector('#idName').remove()
+        }, 4000);
     }
 }
-//Get the password
-var myInput = document.getElementById("psw");
+const form = document.getElementById("form");
+const spans = form.querySelectorAll("span");
+const fnameEspan = spans[1];
+const lnameEspan = spans[2];
+const emailEspan = spans[3];
+const phoneEspan = spans[4];
+const pswEspan = spans[5];
+const psw2Espan = spans[6];
 
-// When the user starts to type something inside the password field
-myInput.onkeyup = function () {
-    // Validate lowercase letters
-    var lowerCaseLetters = /[a-z]/g;
-    if (myInput.value.match(lowerCaseLetters)) {
-        UI.showAlert('A lowerCase letter','valid');
-    } else {
-        UI.showAlert('A lowerCase letter','invalid');
-    }
+// After form loads focus will go to User id field.
+function firstfocus() {
+    const fname = document.signup.fname.focus();
+    return true;
+}
 
-    // Validate capital letters
-    var upperCaseLetters = /[A-Z]/g;
-    if (myInput.value.match(upperCaseLetters)) {
-        UI.showAlert('A upperCase letter','valid');
-    } else {
-        UI.showAlert('A upperCase letter','invalid');
+// This function will validate First Name.
+function fname_validation() {
+    const firstName = document.signup.fname;
+    const letters = /^[A-Za-z]+$/;
+    if (firstName.value.match(letters)) {
+        fnameEspan.className = "active";
+        firstName.setCustomValidity("");
+        document.querySelector('#id-fname').remove()
+        // Focus goes to next field.
+        document.signup.lname.focus();
     }
-
-    // Validate numbers
-    var numbers = /[0-9]/g;
-    if (myInput.value.match(numbers)) {
-        UI.showAlert('A Number','valid');
-    } else {
-        UI.showAlert('A Number','invalid');
+    else {
+        firstName.setCustomValidity("firstName must have alphabet characters only");
+        fnameEspan.className = "active";
+        firstName.focus();
+        const msgDiv = document.getElementById('id-fname');
+        msgDiv.textContent = 'firstName must have alphabet characters only';
+        msgDiv.className = "error-message";
     }
-    
-    // Validate length
-    if (myInput.value.length >= 8) {
-        UI.showAlert('Minimum 8 characters','valid');
-    } else {
-        UI.showAlert('Minimum 8 characters','invalid');
+}
+// This function will validate Last Name.
+function lname_validation() {
+    const lastName = document.signup.lname;
+    const letters = /^[A-Za-z]+$/;
+    if (lastName.value.match(letters)) {
+        lnameEspan.className = "active";
+        lastName.setCustomValidity("");
+        document.querySelector('#id-lname').remove();
+        // Focus goes to next field.
+        document.signup.email.focus();
+    }
+    else {
+        lastName.setCustomValidity("LastName must have alphabet characters only");
+        lnameEspan.className = "active";
+        lastName.focus();
+        const msgDiv = document.getElementById('id-lname');
+        msgDiv.textContent = 'LastName must have alphabet characters only';
+        msgDiv.className = "error-message";
     }
 }
 
-function nameValidate() {
-var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-var name = document.getElementById('fname').value;
-if (!regName.test(name)) {
-    UI.showAlert('Name not current', 'invalid')
-    document.getElementById('fname').focus();
-} else {
-    UI.showAlert('Name is ok', 'valid')
+// This function will validate Email.
+function ValidateEmail() {
+    const email = document.signup.email;
+    const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(emailFormat)) {
+        emailEspan.className = "active";
+        email.setCustomValidity("");
+        document.querySelector('#id-email').remove();
+        document.signup.pnumber.focus();
+    }
+    else {
+        email.setCustomValidity("You have entered an invalid email address!");
+        emailEspan.className = "active";
+        email.focus();
+        const msgDiv = document.getElementById('id-email');
+        msgDiv.textContent = 'You have entered an invalid email address!';
+        msgDiv.className = "error-message";
+    }
 }
+
+// This function will validate Phone Number.
+function phoneNumber() {
+    const pNumber = document.signup.pnumber;
+    const phonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (pNumber.value.match(phonePattern)) {
+        phoneEspan.className = "active";
+        pNumber.setCustomValidity("");
+        document.querySelector('#id-pnumber').remove();
+        document.signup.psw.focus();
+    }
+    else {
+        pNumber.setCustomValidity("You have entered an invalid Phone Number!");
+        phoneEspan.className = "active";
+        pNumber.focus();
+        const msgDiv = document.getElementById('id-pnumber');
+        msgDiv.textContent = 'You have entered an invalid Phone Number!';
+        msgDiv.className = "error-message";
+    }
 }
+
+// This function will validate Password.
+function CheckPassword() {
+    const password = document.signup.psw;
+    const passwordFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (psw.value.match(passwordFormat)) {
+        pswEspan.className = "active";
+        password.setCustomValidity("");
+        document.querySelector('#id-psw').remove();
+        document.signup.psw2.focus();
+    }
+    else {
+        password.setCustomValidity("at least 1 number & 1 uppercase & lowercase letter & at least 8 or more characters");
+        pswEspan.className = "active";
+        password.focus();
+        const msgDiv = document.getElementById('id-psw');
+        msgDiv.textContent = 'at least 1 number, 1 uppercase, 1 lowercase letter, at least 8 or more characters';
+        msgDiv.className = "error-message";
+    }
+}
+
+// This function will confirm Passwords .
 function passValidate() {
+    const pswRepeat = document.signup.psw2;
+    const  password = document.getElementById("psw").value;
+    const confirmPassword = document.getElementById("psw2").value;
+    if (password === confirmPassword) {
+        psw2Espan.className = "active";
+        pswRepeat.setCustomValidity("");
+        document.querySelector('#id-psw2').remove();
+        document.signup.btn.focus();
 
-    var password = document.getElementById("psw").value;
-    var confirmPassword = document.getElementById("psw-repeat").value;
-    if (password != confirmPassword) {
-        UI.showAlert('Passwords did not match', 'invalid')
     } else {
-        UI.showAlert('Password created successfully', 'valid')
+        pswRepeat.setCustomValidity("Passwords must be the same");
+        psw2Espan.className = "active";
+        pswRepeat.focus();
+        const msgDiv = document.getElementById('id-psw2');
+        msgDiv.textContent = 'Passwords must be the same';
+        msgDiv.className = "error-message";
     }
 }
-
-document.querySelector('.login-form').addEventListener('submit', (e) => {
-    //Prevent Actual Submit
-    e.preventDefault();
-    nameValidate();
-    passValidate();
-});
